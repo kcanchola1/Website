a87a7edc34a74fcd6484d327fb7c6e43eca81d3a
Kim Canchola <Kimberly Canchola>
2020-06-09 23:51:25 -0700

    Javascript, firestore, and firebase additions and changes. Log in updated

--- Script1.js
+++ Script1.js
@@ -1,3 +1,35 @@
 // JavaScript source code
 
-alert("Hello, World!");
+
+
+function login() {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+
+            document.getElementById("user_div").style.display = "block";
+            document.getElementById("login_div").style.display = "none";
+
+        } else {
+            // No user is signed in.
+            document.getElementById("login_div").style.display = "block";
+            document.getElementById("user_div").style.display = "none";
+
+        }
+    
+
+    var userEmail = document.getElementById("userEmail");
+    var userPassword = document.getElementById("userPassword");
+    window.alert(userEmail.value + ".." + userPassword.value);
+
+        firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+
+            window.alert(error.message)
+        });
+
+}
